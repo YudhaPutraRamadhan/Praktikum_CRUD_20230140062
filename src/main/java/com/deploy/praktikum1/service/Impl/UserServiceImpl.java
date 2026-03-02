ef50fd0de826ef3bb1a2d9bfaa97df3f22bc4cca
YudhaPutraRamadhan <yudharamadhan838@gmail.com>
2026-03-02 10:05:04 +0700

    Menambahkan class UserServiceImpl

---
+++ src/main/java/com/deploy/praktikum1/service/Impl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.deploy.praktikum1.service.Impl;
+
+import com.deploy.praktikum1.mapper.UserMapper;
+import com.deploy.praktikum1.model.dto.UserAddRequest;
+import com.deploy.praktikum1.model.dto.UserDto;
+import com.deploy.praktikum1.model.entity.User;
+import com.deploy.praktikum1.repository.UserRepository;
+import com.deploy.praktikum1.util.ValidationUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl {
+}
